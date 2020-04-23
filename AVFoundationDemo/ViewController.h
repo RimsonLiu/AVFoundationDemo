e3f2ef89e3d442393c7447594f9ab3609417043b
Rimson Liu <rimson@qq.com>
2020-04-23 18:37:47 +0800

    add cocoa pods

--- AVFoundationDemo/ViewController.h
+++ AVFoundationDemo/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ELCImagePicker/ELCImagePickerHeader.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <ELCImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 
 @end
--- AVFoundationDemo/ViewController.m
+++ AVFoundationDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self initView];
 }
 
+- (void)initView {
+    UIButton *choosePhotoButton = [[UIButton alloc] init];
+    [choosePhotoButton setTitle:@"选择图片" forState:UIControlStateNormal];
+    [choosePhotoButton setBackgroundColor:[UIColor blackColor]];
+    UITapGestureRecognizer *tapChoosePhotoButton = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(choosePhoto)];
+    [choosePhotoButton addGestureRecognizer:tapChoosePhotoButton];
+    [self.view addSubview:choosePhotoButton];
+    [choosePhotoButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
+    }];
+    
+    self.navigationController.navigationBar.hidden = NO;
+}
+
+#pragma mark - Action
+
+- (void)choosePhoto {
+    ELCImagePickerController *imagePickerController = [[ELCImagePickerController alloc] initImagePicker];
+    imagePickerController.maximumImagesCount = INFINITY;
+    imagePickerController.returnsImage = YES;
+    imagePickerController.onOrder = YES;
+    imagePickerController.imagePickerDelegate = self;
+    imagePickerController.modalPresentationStyle = UIModalPresentationFullScreen;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+}
+
+#pragma mark - ELCImagePickerControllerDelegate
+
+
+
+- (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
